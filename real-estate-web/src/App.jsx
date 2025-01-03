ef29b9538c0be4ae17fd3e1cc0b7667c9dd2c939
Kaveesha Rathnayake <kaveesha.20222294@iit.ac.lk>
2025-01-04 00:32:30 +0530

    Initial Commit

---
+++ real-estate-web/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './Components/Header/Header'
+import Testimonial from './Components/Header/Testimonials'
+import Nav from './Components/Nav/Nav'
+
+function App() {
+
+  return (
+    <>
+      <Nav/>
+      <Header/>
+      <Testimonial/>
+    </>
+  )
+}
+
+export default App
